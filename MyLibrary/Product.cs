ccc4849ba67f30d5ba28e14e5508b3964f354559
siver <siver@MAAMBOWIN>
2024-10-17 21:37:17 -0500

    Commit

---
+++ MyLibrary/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Product(int id, string name, decimal unitPrice, int unitsInStock)
+    {
+        //propiedades
+        public int Id => Id;
+        public string Name => name;
+        public decimal UnitPrice => unitPrice;
+        public int UnitsInStock => unitsInStock;
+
+        public bool Discontinued { get; private set; }
+
+        public void SetDiscontinued() =>
+            Discontinued = true;
+
+        public bool GetStatus() =>
+            Discontinued;
+    }
+}
